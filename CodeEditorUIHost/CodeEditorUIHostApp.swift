732c96300217e8c03274e22df1790a0645ba54c7
Miguel de Icaza <miguel@gnome.org>
2024-03-29 14:23:37 -0400

    Initial Commit

---
+++ CodeEditorUIHost/CodeEditorUIHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeEditorUIHostApp.swift
+//  CodeEditorUIHost
+//
+//  Created by Miguel de Icaza on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeEditorUIHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeEditorUIHost/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeEditorUIHost
+//
+//  Created by Miguel de Icaza on 3/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
