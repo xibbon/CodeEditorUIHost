914e61b1eddc7f471df53f196b879ce89e879fc6
Miguel de Icaza <miguel@gnome.org>
2024-04-10 14:24:16 -0400

    Apologies

--- CodeEditorUIHost/ContentView.swift
+++ CodeEditorUIHost/ContentView.swift
@@ -10,8 +10,7 @@ import CodeEditorUI
 import TreeSitter
 import Runestone
 
-struct ContentView: View {
-    @State var state = CodeEditorState (hostServices: HostServices.makeTestHostServices())
+class MyDelegate: EditedItemDelegate {
     
     static func makeTestData () -> [CompletionEntry] {
         return [
@@ -27,7 +26,8 @@ struct ContentView: View {
         ]
     }
     
-    func onChange (_ state: CodeEditorState, _ editedItem: EditedItem, textView: TextView) {
+    func editedTextChanged(_ editedItem: CodeEditorUI.EditedItem, _ textView: Runestone.TextView) {
+        
         let range = textView.selectedRange
         guard range.length == 0 else {
             // We are not interested in doing completions when there is a selection going
@@ -48,11 +48,28 @@ struct ContentView: View {
         let region = textView.firstRect(for: r)
 
         if line.hasSuffix("pri") {
-            editedItem.requestCompletion (at: region, on: textView, prefix: "pri", completions: ContentView.makeTestData())
+            editedItem.requestCompletion (at: region, on: textView, prefix: "pri", completions: MyDelegate.makeTestData())
+            
+            let errors: [Issue] = [
+                Issue(kind: .error, col: 1, line: 2, message: "This is a cute error"),
+                Issue(kind: .error, col: 1, line: 3, message: "Another error")
+            ]
+            let warnings: [Issue] = [
+                Issue(kind: .error, col: 1, line: 5, message: "Warning"),
+                Issue(kind: .error, col: 1, line: 6, message: "I am telling you")
+            ]
+
+            editedItem.validationResult(functions: [("DemoAtLine10", 10), ("AnotherAtLine20", 20)], errors: errors, warnings: warnings)
         } else {
             editedItem.cancelCompletion()
         }
     }
+}
+
+struct ContentView: View {
+    @State var state = CodeEditorState (hostServices: HostServices.makeTestHostServices())
+    @State var delegate = MyDelegate ()
+    
     
     var body: some View {
         VStack {
@@ -66,14 +83,22 @@ struct ContentView: View {
                 Button ("Toggle Line#") {
                     state.showLines.toggle()
                 }
+                Button ("Search") {
+                    state.search(showReplace: false)
+                }
+                Button ("Goto Line 1") {
+                    state.goTo(line: 0)
+                }
+
             }
             ZStack {
                 Color.yellow
                 CodeEditorShell(state: $state)
                     .padding()
                     .onAppear {
-                        _ = state.openFile(path: "/Users/miguel/cvs/godot-master/modules/gdscript/tests/scripts/utils.notest.gd", data: nil)
-                        state.onChange = onChange
+                        _ = state.openFile(path: "/Users/miguel/cvs/godot-master/modules/gdscript/tests/scripts/utils.notest.gd", delegate: delegate)
+                        _ =  state.openFile(path: "/Users/miguel/cvs/godot-master/modules/gdscript//editor/script_templates/Object/empty.gd", delegate: nil)
+
                     }
             }
         }
